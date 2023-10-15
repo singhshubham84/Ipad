658aa94d26f2caac8add065bdda0867e27cc2b08
singhshubham84 <shubham333singh@gmail.com>
2023-10-15 19:55:25 +0000

    new

--- node.js
+++ node.js
@@ -1,4 +1,15 @@
-console.log("hello")
+var d=new Date();
+theDay=d.getDay();
+switch (theDay)
+{
+case 5:document.write("Finally Friday");
+break; case 6:
+document.write("Super Saturday");
+break; case 0:
+document.write("Sleepy Sunday");//console.log("hello")
+break; default:
+document.write("I'm looking forward to this weekend!"); }
 let a=0
 let b= Boolean(a)
-console.log(b)
+//console.log(b)//
+
